69ccab390e969c035720b096949cd062df6c0a9f
Oliver Ulfik <oliver.ulfik@itk-engineering.de>
2022-11-22 10:41:16 +0100

    change deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,7 @@ docker container run -d \
 # Logic to wait for the api component to be ready on port 3000
 
 read -d '' wait_for << EOF
+#!/bin/bash
 echo "Waiting for API to listen on port 3000..."
 while ! nc -z api 3000; do
     sleep 0.1 # wait for 1/10 of the second before check again
