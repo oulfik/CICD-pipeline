358a70b13a6031d22cd57d9e03f4fce20454f26f
Oliver Ulfik <oliver.ulfik@itk-engineering.de>
2022-11-25 10:39:50 +0100

    print json

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -4,6 +4,7 @@ def call(String buildStatus, String buildName, Map config){
     println(buildStatus)
     println(config)
     def mailConfigJSON =  JsonOutput.toJson(config.mail_settings)
+    println(mailConfigJSON)
 
 
 
