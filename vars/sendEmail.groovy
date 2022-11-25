f402d8d0cd8897f59bca0e41d6146b02822452d2
Oliver Ulfik <oliver.ulfik@itk-engineering.de>
2022-11-25 10:38:21 +0100

    try with json

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,6 +1,12 @@
-def call(String buildStatus, Map mailConfig){
+import groovy.json.*
+
+def call(String buildStatus, String buildName, Map config){
     println(buildStatus)
     println(mailConfig)
+    def mailConfigJSON =  JsonOutput.toJson(config.mail_settings)
+
+
+
     echo 'executed!'
     return 'someEmail@gmail.com'
 }
