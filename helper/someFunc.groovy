b4207d86ae5fbbcde5ed653ff27aa7c057577a63
Oliver Ulfik <oliver.ulfik@itk-engineering.de>
2022-11-25 16:18:58 +0100

    try sth

--- helper/someFunc.groovy
+++ helper/someFunc.groovy
@@ -1,13 +1,5 @@
-def mail(String arg1) {
-        sh """
-        echo "from build function : ${arg1}"
-        """
-}
-
-def deploy(String arg2) {
-        sh """
-        echo "from deploy function : ${arg2}"
-        """
+def sendMails(){
+    echo 'sent mail'
 }
 
 return this
